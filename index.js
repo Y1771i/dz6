c685e573d6c9f1a07003ebc18e15e56d772d2f0a
Y1771i <abdulilov654@gmail.com>
2024-11-23 18:14:45 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,1 @@
-export { Welcome } from './Welcome';
-export { StepOne } from './StepOne';
-export { StepTwo } from './StepTwo';
-export { StepThree } from './StepThree';
-export { StepFour } from './StepFour';
-export { Thanks } from './Thanks';
+export { Progress } from './Progress'
